45aafb6dd3b2a616ee4f4d91a54d9ab065585dc3
EdwardMuturi <kakaturi300@gmail.com>
2021-04-22 13:35:50 +0300

    set splash screen to fullscreen, set spash background to blue_500 and splash text to green_300

--- app/src/main/java/com/mementoguy/pulavey/SplashActivity.kt
+++ app/src/main/java/com/mementoguy/pulavey/SplashActivity.kt
@@ -4,11 +4,16 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.view.Window
+import android.view.WindowManager
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_splash)
+        actionBar?.hide()
 
         navigateToMainActivity()
     }
@@ -17,6 +22,6 @@ class SplashActivity : AppCompatActivity() {
         val mainActivityIntent = Intent(this, MainActivity::class.java)
         Handler().postDelayed({
             startActivity(mainActivityIntent)
-        }, 1000)
+        }, 1200)
     }
 }
