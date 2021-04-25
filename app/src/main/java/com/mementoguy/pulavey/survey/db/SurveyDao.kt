095e32ff909738cafb16b5fbb14b193fb776a3b7
EdwardMuturi <kakaturi300@gmail.com>
2021-04-25 17:36:12 +0300

    add query methods, findAllSurveys and findSurveyById(Id:String)

--- app/src/main/java/com/mementoguy/pulavey/survey/db/SurveyDao.kt
+++ app/src/main/java/com/mementoguy/pulavey/survey/db/SurveyDao.kt
@@ -3,6 +3,7 @@ package com.mementoguy.pulavey.survey.db
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
 import com.mementoguy.pulavey.survey.model.Survey
 
 /**
@@ -11,6 +12,13 @@ import com.mementoguy.pulavey.survey.model.Survey
 
 @Dao
 interface SurveyDao {
+
+    @Query("SELECT * FROM Survey")
+    fun findAllSurveys() : List<Survey>
+
+    @Query("SELECT * FROM Survey WHERE id =:Id")
+    fun findSurveyById(Id : String) : Survey
+
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun saveSurvey(survey: Survey)
 }
