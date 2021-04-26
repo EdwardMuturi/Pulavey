517b21e4ba105965bedca90160e8590ae8c00621
EdwardMuturi <kakaturi300@gmail.com>
2021-04-26 09:00:19 +0300

    start responsesSyncWorker on SurveyViewModel creation

--- app/src/main/java/com/mementoguy/pulavey/survey/ui/SurveyViewModel.kt
+++ app/src/main/java/com/mementoguy/pulavey/survey/ui/SurveyViewModel.kt
@@ -4,16 +4,15 @@ import android.app.Application
 import android.content.Context
 import android.util.Log
 import androidx.lifecycle.*
-import androidx.work.Constraints
-import androidx.work.OneTimeWorkRequestBuilder
-import androidx.work.WorkManager
-import androidx.work.workDataOf
+import androidx.work.*
 import com.google.gson.Gson
 import com.mementoguy.pulavey.survey.data.SurveyRepository
 import com.mementoguy.pulavey.survey.model.Question
 import com.mementoguy.pulavey.survey.model.Response
 import com.mementoguy.pulavey.survey.workers.SaveResponsesWorker
+import com.mementoguy.pulavey.survey.workers.SyncOfflineResponsesWorker
 import kotlinx.coroutines.launch
+import java.util.concurrent.TimeUnit
 
 /**
  * Created by Edward Muturi on 24/04/2021.
@@ -23,6 +22,10 @@ class SurveyViewModel(private val surveyRepository: SurveyRepository, applicatio
     private val mutableQuestions: MutableLiveData<List<Question>> = MutableLiveData()
     val questions : LiveData<List<Question>> get() = mutableQuestions
     private val workManager= WorkManager.getInstance(application)
+
+    init {
+        uploadResponses()
+    }
 
     fun syncDataFromServer(){
         viewModelScope.launch {
@@ -43,7 +46,19 @@ class SurveyViewModel(private val surveyRepository: SurveyRepository, applicatio
             .setInputData(responsesData)
             .build()
 
-        workManager.enqueue(saveResponseWorkRequest)
+        workManager.beginUniqueWork("SAVE_RESPONSES_WORK", ExistingWorkPolicy.KEEP ,saveResponseWorkRequest).enqueue()
+    }
+
+    private fun uploadResponses(){
+        val constraints= Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.CONNECTED)
+            .build()
+
+        val uploadResponsesWorker= PeriodicWorkRequestBuilder<SyncOfflineResponsesWorker>(15, TimeUnit.MINUTES)
+            .setConstraints(constraints)
+            .build()
+
+        workManager.enqueueUniquePeriodicWork("RESPONSE_SYNC_WORK", ExistingPeriodicWorkPolicy.KEEP, uploadResponsesWorker)
     }
 
 }
