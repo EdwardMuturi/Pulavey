4731d0828239f9ad2f24c196f753d51e6d3861d5
EdwardMuturi <kakaturi300@gmail.com>
2021-08-14 22:16:17 +0300

    populate surveyViewPager in SurveryFragment, - inject SurveyViewModel into SurveyFragment as sharedViewModel

--- app/src/main/java/com/mementoguy/pulavey/ui/SurveyActivity.kt
+++ app/src/main/java/com/mementoguy/pulavey/ui/SurveyActivity.kt
@@ -16,10 +16,8 @@ import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class SurveyActivity : AppCompatActivity() {
 
-    private val questionnareViewPagerAdapter = QuestionnareViewPagerAdapter()
     private lateinit var binding: ActivitySurveyBinding
     private val surveyViewModel: SurveyViewModel by viewModel()
-    private val loadingDialog= LoadingDialog(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,12 +25,8 @@ class SurveyActivity : AppCompatActivity() {
         binding = ActivitySurveyBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-//        loadData()
-//        displayQuestions()
-//        getResponseInput()
-//        finishSurvey()
+        loadData()
     }
-
 
     private fun loadData(){
         // assumes list of survey and position represents selected survey
@@ -49,66 +43,6 @@ class SurveyActivity : AppCompatActivity() {
             }
         }
 
-    }
-
-    private fun displayQuestions() {
-        surveyViewModel.questions.observe(this, Observer { questions ->
-            setUpQuestionnareViewPager(questions)
-            displayNextQuestion(questions)
-        })
-    }
-
-    private fun setUpQuestionnareViewPager(questions: List<Question>) {
-        binding.vpSurvey.isUserInputEnabled= false
-        binding.vpSurvey.adapter = questionnareViewPagerAdapter
-        questionnareViewPagerAdapter.submitList(questions)
-    }
-
-    private fun displayNextQuestion(questions: List<Question>) {
-        with(binding) {
-            btnNext.setOnClickListener {
-                if (vpSurvey.currentItem != questions.size - 1)
-                    vpSurvey.currentItem++
-                else {
-                   hideNextButton()
-                }
-            }
-        }
-    }
-
-    private fun hideNextButton(){
-        binding.btnFinish.visibility = View.VISIBLE
-        binding.btnNext.visibility = View.GONE
-    }
-
-    private fun showNextButton(){
-        binding.btnNext.visibility = View.VISIBLE
-        binding.btnFinish.visibility = View.GONE
-    }
-
-    private fun finishSurvey(){
-        binding.btnFinish.setOnClickListener {
-            loadingDialog.startLoadingDialog()
-            saveResponses()
-            dismissLoader()
-        }
-    }
-
-    private fun saveResponses(){
-        val responses= listOf(Response(1,"surv1", "Qtn1", "yes"))
-        surveyViewModel.saveResponses(responses)
-    }
-
-    private fun dismissLoader(){
-        Handler().postDelayed({
-            Toast.makeText(this, "Responses saved successfully", Toast.LENGTH_SHORT).show()
-            binding.vpSurvey.currentItem= 0
-            loadingDialog.dismissDialog()
-            showNextButton()
-        }, 1000)
-    }
-
-    private fun getResponseInput(){
     }
 
 }
--- app/src/main/java/com/mementoguy/pulavey/ui/SurveyFragment.kt
+++ app/src/main/java/com/mementoguy/pulavey/ui/SurveyFragment.kt
@@ -1,26 +1,33 @@
 package com.mementoguy.pulavey.ui
 
 import android.os.Bundle
+import android.os.Handler
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.Observer
 import com.mementoguy.pulavey.R
+import com.mementoguy.pulavey.databinding.FragmentSurveyBinding
+import com.mementoguy.pulavey.model.Question
+import com.mementoguy.pulavey.model.Response
+import com.mementoguy.pulavey.questionnaire.QuestionnareViewPagerAdapter
+import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [SurveyFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SurveyFragment : Fragment() {
-    // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+
+    private var mutableBinding: FragmentSurveyBinding? = null
+    private val binding get() = mutableBinding!!
+
+    private val questionnareViewPagerAdapter = QuestionnareViewPagerAdapter()
+    private val loadingDialog= LoadingDialog(requireActivity())
+    private val surveyViewModel by sharedViewModel<SurveyViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,24 +37,56 @@ class SurveyFragment : Fragment() {
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_survey, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        mutableBinding= FragmentSurveyBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    private fun setUpQuestionnareViewPager(questions: List<Question>) {
+        binding.vpSurvey.isUserInputEnabled= false
+        binding.vpSurvey.adapter = questionnareViewPagerAdapter
+        questionnareViewPagerAdapter.submitList(questions)
+    }
+
+    private fun displayQuestions() {
+        surveyViewModel.questions.observe(this, Observer { questions ->
+            setUpQuestionnareViewPager(questions)
+            displayNextQuestion(questions)
+        })
+    }
+
+    private fun displayNextQuestion(questions: List<Question>) {
+        binding.vpSurvey.currentItem++
+    }
+
+
+//    private fun finishSurvey(){
+//        binding.btnFinish.setOnClickListener {
+//            loadingDialog.startLoadingDialog()
+//            saveResponses()
+//            dismissLoader()
+//        }
+//    }
+
+    private fun saveResponses(){
+        val responses= listOf(Response(1,"surv1", "Qtn1", "yes"))
+        surveyViewModel.saveResponses(responses)
+    }
+
+    private fun dismissLoader(){
+        Handler().postDelayed({
+            Toast.makeText(requireContext(), "Responses saved successfully", Toast.LENGTH_SHORT).show()
+            binding.vpSurvey.currentItem= 0
+            loadingDialog.dismissDialog()
+        }, 1000)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        mutableBinding= null
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SurveyFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             SurveyFragment().apply {
